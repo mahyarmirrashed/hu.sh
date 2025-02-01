35804b5d397e1d375cf6ab7eae0967522ce2ddb0
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-01 13:28:52 -0600

    feat: add secrets table

---
+++ server/src/db/migrations/20250201132200_secrets.js
@@ -0,0 +1,33 @@
+const {
+  createOnUpdateTrigger,
+  dropOnUpdateTrigger,
+} = require("../util/db-util");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  if (!(await knex.schema.hasTable("secrets"))) {
+    await knex.schema.createTable("secrets", (t) => {
+      // The short identifier (exactly 8 characters)
+      t.string("shortId", 8).primary();
+      // Expiration date in Zulu time
+      t.timestamp("expiresAt").notNullable();
+      // The fragments from shamirs-secret-sharing (stored as a JSON array)
+      t.jsonb("fragments").notNullable();
+      // Optional password hash (null if no password)
+      t.string("hash");
+    });
+  }
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  if (await knex.schema.hasTable("secrets")) {
+    await knex.schema.dropTable("secrets");
+  }
+};
