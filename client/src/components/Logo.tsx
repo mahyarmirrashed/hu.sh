9b8c933cce73fc4d25fd0d825fb31a5acec7e6e8
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-04 19:34:45 -0600

    style: add hover effect to logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Logo = () => (
   <Link
     to="/"
-    className="absolute bottom-6 left-6 text-white text-3xl font-bold"
+    className="absolute bottom-6 left-6 text-gray-500 hover:text-gray-300 text-xl font-bold"
   >
     smol.lnk
   </Link>
